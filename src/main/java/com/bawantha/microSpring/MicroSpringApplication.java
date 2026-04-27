bffd2f78826f4d04a94d2bcffe1af257967b5521
bawantha <bawantha.inbox@gmail.com>
2026-04-27 10:50:01 +1000

    first commit

---
+++ src/main/java/com/bawantha/microSpring/MicroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bawantha.microSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bawantha/microSpring/MicroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bawantha.microSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
