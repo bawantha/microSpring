6cccd30147a9eb2170f7176d71235520773a4208
bawantha <bawantha.inbox@gmail.com>
2026-04-27 10:59:20 +1000

    feat: add Cart entity for MongoDB storage

---
+++ src/main/java/com/bawantha/microSpring/entity/Cart.java
@@ -0,0 +1,23 @@
+package com.bawantha.microSpring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "carts")
+public class Cart {
+
+    @Id
+    private String id;
+    private List<CartItem> items;
+
+}
